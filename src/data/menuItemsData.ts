04228633c3c48504a6424091d2434cd3542bc716
Nahuel Borromeo <nahuelborromeo@terminus.local>
2023-11-08 16:07:51 -0500

    first commit

---
+++ src/data/menuItemsData.ts
@@ -0,0 +1,30 @@
+import type { MenuItem } from "../types/types";
+
+const menuItems: MenuItem[] = [
+    {
+        title: 'Inicio',
+        url: '/'
+    },
+    {
+        title: 'Nosotros',
+        url: '/nosotros'
+    },
+    {
+        title: 'Servicios',
+        url: '/servicios'
+    },
+    {
+        title: 'Contáctanos',
+        url: '/contacto'
+    },
+    {
+        title: 'Blog',
+        url: '/blog'
+    },
+    {
+        title: 'Preguntas Frecuentes',
+        url: '/faq'
+    },
+]
+
+export default menuItems;
